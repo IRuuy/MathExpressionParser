dd5cca014afea42951f2b374f9b98929287b3f7a
Daniil Shulgin <manganeko@yandex.ru>
2024-03-20 16:45:34 +0700

    refactor(token): change regex for IDENTIFIER

--- src/main/java/ru/shulgindaniil/token/type/impl/Identifier.java
+++ src/main/java/ru/shulgindaniil/token/type/impl/Identifier.java
@@ -3,7 +3,7 @@ package ru.shulgindaniil.token.type.impl;
 import ru.shulgindaniil.token.type.TokenType;
 
 public enum Identifier implements TokenType {
-    IDENTIFIER("([A-Za-zА-Яа-я0-9_]*)[\\s]*");
+    IDENTIFIER("[A-Za-zА-Яа-я0-9_]*");
 
     private final String regex;
     Identifier(String regex) {
